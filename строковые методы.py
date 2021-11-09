eb4579531ba3ff776f4b9652a1b6f1744ffe0f0a
Gracheva Anastasia <anastasyagrachyova@gmail.com>
2021-11-09 18:50:19 +0500

    inital repo

---
+++ строковые методы.py
@@ -0,0 +1,11 @@
+quote = '   Суть жизни - найти себя   '
+print(quote)
+print(quote.upper())
+print(quote.lower())
+print(quote.title())
+print(quote.swapcase())
+print(quote.capitalize())
+print(quote.strip())
+print(quote.replace('Суть', 'Цель'))
+
+input('Нажмите Enter, чтобы закрыть')
